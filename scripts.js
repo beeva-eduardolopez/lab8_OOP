005f40c481e4420c1f50cba38bd799072ab9feb9
beeva-eduardolopez <eduardo.lopez.anta@bbva.com>
2016-03-05 19:10:04 -0600

    v2 with OOP

--- scripts.js
+++ scripts.js
@@ -16,14 +16,16 @@ function rollDice() {
     }
 }
 function createDice() {
-    // var id = dices.length;
-    // var html = '<div class="dice" id="'+ id +'">0</div>';
-    // var dice = new Dice(html);
-    
-    var dice = document.createElement('div');
-    dice.className = 'dice';
-    dice.id = dices.length;
-    dice.innerHTML = "<id='" + dice.id + ">0</p>";
-    document.body.appendChild(dice);
+    var id = dices.length;
+    var html = '<div class="dice" id="'+ id +'">0</div>';
+    var diceOOP = new Dice(html);
+    document.getElementById("dices-container").innerHTML+= diceOOP.value;
+   
+   
+    // var dice = document.createElement('div');
+    // dice.className = 'dice';
+    // dice.id = dices.length;
+    // dice.innerHTML = "<id='" + dice.id + ">0</p>";
+    // document.body.appendChild(dice);
 
 }
